51efe3d0c8c5357321bf5a3afdbf36c26a67f283
root <root@.localdomain>
2022-09-10 03:38:05 +0530

    random number generator

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<time.h>
+/**
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand();
+
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n > 0)
+{
+	printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+	printf("0 is zero\n");
+}
+return (0);
+}
