b3f0e1e5349bfae7db9dc4f7d34e7886e1780350
root <root@.localdomain>
2022-09-09 20:29:35 +0530

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+ #include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
-{   
+{
 char *string = "\"Programming is like building a multilingual puzzle";
 puts(string);
 return (0);
-} 
-			
+}
