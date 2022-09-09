7ddfc4fc8db6e38a3b09d3925f67fd65406b202c
root <root@.localdomain>
2022-09-09 23:32:33 +0530

    print the data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,13 @@ int main(void)
 char c;
 int i;
 long l;
+long int li;
 float f;
 printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of a int: %d byte(s)\n", sizeof(i));
-printf("Size of a long: %d byte(s)s\n", sizeof(l));
-printf("Size of a float: %d byre(s)\n", sizeof(f));
+printf("Size of a an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long long int: %d byte(s)\n", sizeof(li));
+printf("Size of a long: %d byte(s)\n", sizeof(l));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
 
