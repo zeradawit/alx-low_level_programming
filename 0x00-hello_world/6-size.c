d63d04ff963ce5ab451fd10fe1d9556606bfddb1
root <root@.localdomain>
2022-09-09 23:25:21 +0530

    print the data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
+int main(void)
+{
+char c;
+int i;
+long l;
+float f;
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of a int: %d byte(s)\n", sizeof(i));
+printf("Size of a long: %d byte(s)s\n", sizeof(l));
+printf("Size of a float: %d byre(s)\n", sizeof(f));
+return (0);
+}
+
