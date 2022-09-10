f21406d6a377530140b1c70885cafb3c56c9432d
root <root@.localdomain>
2022-09-10 23:19:51 +0530

    print numbers by intializing intiger variale

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
+int main(void)
+{
+	int ch = '0';
+
+	for (; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
