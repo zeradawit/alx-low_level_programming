2662473e39ceb4651f913a03b4d8ed290410772c
root <root@.localdomain>
2022-09-10 07:10:13 +0530

    Im happy with this

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+   * main - Entry point
+   *
+    * Return: Always 0 (Success)
+     */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+char CH = 'A';
+for ('A'; CH <= 'Z'; CH++)
+putchar(CH);
+putchar('\n');
+return (0);
+}
