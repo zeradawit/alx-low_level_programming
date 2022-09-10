2c9a5644f88379f79c1f42a369841adf13256140
root <root@.localdomain>
2022-09-10 19:33:30 +0530

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,22 +6,23 @@
      * Return: Always 0 (Success)
       */
 int main(void)
-{	
-int n;	
-last = n % 10;
-n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d and is ", n, last);
-if (n == 0)
 {
-printf("0\n");
-}
-else if (n > 5)
-{	
-printf("greater than 5\n");
-}
-else if (n < 0)
-{	
-printf("less than 6 and not 0\n");
-}
-return (0);
+	int n;
+	int last = n % 10;
+
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d and is ", n, last);
+	if (n == 0)
+	{
+		printf("0\n");
+	}
+	else if (n > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (n < 0)
+	{
+		printf("less than 6 and not 0\n");
+	}
+	return (0);
 }
